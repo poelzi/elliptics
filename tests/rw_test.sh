4a0f6d1231aca973409e28248e554e198ad54a88
Daniel Poelzleithner <git@poelzi.org>
2009-09-18 05:39:05 +0200

    add tests for transformation
    restructured the tests so they can be selected easily
    add seperators to the server logs for overview
    tests are failing, but shouldn't

--- tests/rw_test.sh
+++ tests/rw_test.sh
@@ -6,15 +6,15 @@
 server_num=2
 server_id_1=00
 server1_addr=127.0.0.1:1025:2
-server_id_2=ff
+server_id_2=ab
 server2_addr=127.0.0.1:1030:2
-file_id_1=99
+file_id_1=44
 
 tmpdir=`prepare_root`
 
 log_file=$tmpdir/log
-tmp_file=$tmpdir/tmp_1
-res_file=$tmpdir/res_1
+tmp_file=$tmpdir/test_1_tmp
+res_file=$tmpdir/test_1_res
 
 trap kill_servers EXIT
 
@@ -22,129 +22,181 @@ echo "rw_test ($tmpdir):"
 
 generate_random_file $tmp_file
 
-# ***************************************
-# * 1 server - 1 client                 *
-# ***************************************
-echo "1 server - 1 client"
+function test_1 {
+    # ***************************************
+    # * 1 server - 1 client                 *
+    # ***************************************
+    log_start "1 server - 1 client"
 
-#start first server
-start_server $server1_addr 0 "$server_opt -i $server_id_1"
-serever1_pid=last_pid
+    #start first server
+    start_server $server1_addr 0 "$server_opt -i $server_id_1"
+    serever1_pid=last_pid
 
-write_data 0 0 0 $server1_addr $tmp_file $file_id_1
-read_data 1 0 0 $server1_addr $res_file $file_id_1
-cmp_files $tmp_file $res_file
+    write_data 0 0 0 $server1_addr $tmp_file $file_id_1
+    read_data 1 0 0 $server1_addr $res_file $file_id_1
+    cmp_files $tmp_file $res_file
 
-#clean  
-kill_servers
-rm $res_file*
+    #clean  
+    kill_servers
+    rm $res_file*
 
-print_passed
+    print_passed
+}
 
-# ***************************************
-# * 1 server - 1 client                 *
-# *  send file in 2 transactions        *
-# ***************************************
-echo "1 server - 1 client"
-echo " send file in 2 transactions"
+function test_2 {
+    # ***************************************
+    # * 1 server - 1 client                 *
+    # *  send file in 2 transactions        *
+    # ***************************************
+    log_start "1 server - 1 client"
+    echo " send file in 2 transactions"
 
-total_size=$(stat --printf="%s" $tmpdir/tmp_1)
-first_trans_size=$(($total_size/2))
-second_trans_size=$(($first_trans_size+($total_size%2)))
+    total_size=$(stat --printf="%s" $tmp_file)
+    first_trans_size=$(($total_size/2))
+    second_trans_size=$(($first_trans_size+($total_size%2)))
 
-#start first server
-start_server $server1_addr 0 "$server_opt -i $server_id_1"
-serever1_pid=last_pid
+    #start first server
+    start_server $server1_addr 0 "$server_opt -i $server_id_1"
+    serever1_pid=last_pid
 
-write_data 0 $first_trans_size 0 $server1_addr $tmp_file $file_id_1
-write_data 0 $second_trans_size $first_trans_size $server1_addr $tmp_file $file_id_1
-read_data 1 0 0 $server1_addr $res_file $file_id_1
-cmp_files $tmp_file $res_file
+    write_data 0 $first_trans_size 0 $server1_addr $tmp_file $file_id_1
+    write_data 0 $second_trans_size $first_trans_size $server1_addr $tmp_file $file_id_1
+    read_data 1 0 0 $server1_addr $res_file $file_id_1
+    cmp_files $tmp_file $res_file
 
-#clean  
-kill_servers
-rm $res_file*
+    #clean  
+    kill_servers
+    rm $res_file*
 
-print_passed
+    print_passed
+}
 
-# ***************************************
-# * 2 server - 1 client                 *
-# *  send request to old first  server  *
-# ***************************************
-echo "2 server - 1 client"
-echo " send request to old first  server"
+function test_3 {
+    # ***************************************
+    # * 2 server - 1 client                 *
+    # *  send request to old first  server  *
+    # ***************************************
+    log_start "2 server - 1 client"
+    echo " send request to old first  server"
 
-#start first server
-start_server $server1_addr 0 "$server_opt -i $server_id_1"
-serever1_pid=last_pid
+    #start first server
+    start_server $server1_addr 0 "$server_opt -i $server_id_1"
+    serever1_pid=last_pid
 
-#start second server
-start_server $server2_addr 0 "$server_opt -i $server_id_2 -r $server1_addr"
-serever2_pid=last_pid
+    #start second server
+    start_server $server2_addr 1 "$server_opt -i $server_id_2 -r $server1_addr"
+    serever2_pid=last_pid
 
-write_data 0 0 0 $server1_addr $tmp_file $file_id_1
-read_data 1 0 0 $server1_addr $res_file $file_id_1
-cmp_files $tmp_file $res_file
+    write_data 0 0 0 $server1_addr $tmp_file $file_id_1
+    read_data 1 0 0 $server1_addr $res_file $file_id_1
+    cmp_files $tmp_file $res_file
 
-#clean  
-kill_servers
-rm $res_file*
+    #clean  
+    kill_servers
+    rm $res_file*
 
-print_passed
+    print_passed
+}
 
-# ***************************************
-# * 2 server - 1 client                 *
-# *  send request to new second server  *
-# ***************************************
-echo "2 server - 1 client"
-echo " send request to new second server"
+function test_4 {
+    # ***************************************
+    # * 2 server - 1 client                 *
+    # *  send request to new second server  *
+    # ***************************************
+    log_start "2 server - 1 client"
+    echo " send request to new second server"
 
-#start first server
-start_server $server1_addr 0 "$server_opt -i $server_id_1"
-serever1_pid=last_pid
+    #start first server
+    start_server $server1_addr 0 "$server_opt -i $server_id_1"
+    serever1_pid=last_pid
 
-#start second server
-start_server $server2_addr 0 "$server_opt -i $server_id_2 -r $server1_addr"
-serever2_pid=last_pid
+    #start second server
+    start_server $server2_addr 1 "$server_opt -i $server_id_2 -r $server1_addr"
+    serever2_pid=last_pid
 
-write_data 0 0 0 $server1_addr $tmp_file $file_id_1
-read_data 1 0 0 $server2_addr $res_file $file_id_1
-cmp_files $tmp_file $res_file
+    write_data 0 0 0 $server1_addr $tmp_file $file_id_1
+    read_data 1 0 0 $server2_addr $res_file $file_id_1
+    cmp_files $tmp_file $res_file
 
-#clean  
-kill_servers
-rm $res_file*
+    #clean  
+    kill_servers
+    rm $res_file*
 
-print_passed
+    print_passed
+}
 
-# ***************************************
-# * 1 server - 1 client                 *
-# *  kill first server and              *
-# *  send request to second server      *
-# *  (simple join test)                 *
-# ***************************************
-echo "2 server - 1 client ====="
-echo " kill first server and"
-echo " send request to second server"
-echo " (simple join test)"
+function test_5 {
+    # ***************************************
+    # * 2 server - 1 client - 2 copies      *
+    # *  send request to new second server  *
+    # ***************************************
+    log_start "2 server - 1 client - 2 copies"
+    echo " send request to new second server"
 
-#start first server
-start_server $server1_addr 0 "$server_opt -i $server_id_1"
-serever1_pid=last_pid
+    #start first server
+    start_server $server1_addr 0 "$server_opt -i $server_id_1" 1
+    serever1_pid=last_pid
 
-write_data 0 0 0 $server1_addr $tmp_file $file_id_1
+    #start second server
+    start_server $server2_addr 1 "$server_opt -i $server_id_2 -r $server1_addr" 1
+    serever2_pid=last_pid
 
-#start second server
-start_server $server2_addr 0 "$server_opt -i $server_id_2 -r $server1_addr"
-serever2_pid=last_pid
+    write_data 0 0 0 $server1_addr $tmp_file -1 1
+    read_data 1 0 0 $server2_addr $res_file -1 1
+    cmp_files $tmp_file $res_file
+    rm $res_file*
 
-read_data 1 0 0 $server2_addr $res_file $file_id_1
-cmp_files $tmp_file $res_file
+    sleep 2
+    kill $serever1_pid
+    # get data from second server
+    read_data 2 0 0 $server2_addr $res_file -1 1
+    cmp_files $tmp_file $res_file
 
-#clean  
-kill_servers
-rm $res_file*
+    #clean  
+    kill_servers
+    rm -f $res_file*
 
-print_passed
+    print_passed
 
-rm -rf $tmpdir
+}
+
+function test_6 {
+    # ***************************************
+    # * 1 server - 1 client                 *
+    # *  kill first server and              *
+    # *  send request to second server      *
+    # *  (simple join test)                 *
+    # ***************************************
+    log_start "2 server - 1 client"
+    echo " kill first server and"
+    echo " send request to second server"
+    echo " (simple join test)"
+
+    #start first server
+    start_server $server1_addr 0 "$server_opt -i $server_id_1"
+    serever1_pid=last_pid
+
+    write_data 0 0 0 $server1_addr $tmp_file $file_id_1
+
+    #start second server
+    start_server $server2_addr 1 "$server_opt -i $server_id_2 -r $server1_addr"
+    serever2_pid=last_pid
+
+    read_data 1 0 0 $server2_addr $res_file $file_id_1
+    cmp_files $tmp_file $res_file
+
+    #clean  
+    kill_servers
+    rm $res_file*
+
+    print_passed
+
+    rm -rf $tmpdir
+}
+
+test_1
+test_2
+test_3
+test_4
+test_5
+test_6
